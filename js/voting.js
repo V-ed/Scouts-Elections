b72d7f71c283665505039fd286b3c4528e2df4eb
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-10-02 20:48:12 -0400

    Implement voting/unvoting on candidates
    Also added a start of an overlay on submit. Submitting does not work yet tho.

--- js/voting.js
+++ js/voting.js
@@ -15,31 +15,36 @@ var tempData = {
 	candidates: tempCandidates
 }
 
+var voteIndexes = [];
+
 function setup_voting_session(data) {
 	
 	var cardsHtml = "";
 	
 	for(var i = 1; i <= data.candidates.length; i++){
 		
+		var candidateIndex = i - 1;
+		
 		// Create rows for each 4 cards
-		if ((i - 1) % 4 == 0) {
+		if (candidateIndex % 4 == 0) {
 			cardsHtml += `<div class="row d-flex justify-content-center my-2">`;
 		}
 		
-		var candidateData = data.candidates[i - 1];
+		var candidateData = data.candidates[candidateIndex];
 		
 		cardsHtml += `
 		<div class="col-3">
 			<div class="card">
 				<div class="card-body text-center">
 					<h5 class="card-title">${candidateData.name}</h5>
-					<button id="vote-candidate-${i}" type="button" class="btn btn-primary">Voter</button>
+					<button id="vote-candidate-${i}" type="button" class="btn btn-primary" data-voterindex="${candidateIndex}" data-voternumber="${i}">Voter</button>
+					<button id="unvote-candidate-${i}" type="button" class="btn btn-danger" data-voterindex="${candidateIndex}" data-voternumber="${i}" hidden>Enlever Vote</button>
 				</div>
 			</div>
 		</div>`;
 		
 		// End rows for each 4 cards
-		if ((i - 1) % 4 == 3) {
+		if (candidateIndex % 4 == 3) {
 			cardsHtml += `</div>`;
 		}
 		
@@ -51,6 +56,66 @@ function setup_voting_session(data) {
 	
 	var voteRemainingCounter = document.getElementById("voting-remaining-count");
 	
-	voteRemainingCounter.textContent = data.numberOfVotePerVoter;
+	var numberOfVotesLeft = data.numberOfVotePerVoter;
+	
+	voteRemainingCounter.textContent = numberOfVotesLeft;
+	
+	var submitVotesButton = document.getElementById("voting-submit-button");
+	
+	var votingButtons = document.querySelectorAll("button[id^=vote-candidate-]");
+	
+	votingButtons.forEach(function (button) {
+		button.addEventListener("click", function (e) {
+			e.preventDefault();
+			
+			voteIndexes.push(button.dataset.voterindex);
+			
+			button.hidden = true;
+			document.getElementById(`unvote-candidate-${button.dataset.voternumber}`).hidden = false;
+			
+			voteRemainingCounter.textContent = --numberOfVotesLeft;
+			
+			if(numberOfVotesLeft == 0){
+				
+				var nonVotedCandidatesButtons = document.querySelectorAll("button[id^=vote-candidate-]:not([hidden])");
+				
+				nonVotedCandidatesButtons.forEach(button => button.disabled = true);
+				
+				submitVotesButton.disabled = false;
+				
+			}
+			
+		});
+	});
+	
+	var unvoteButtons = document.querySelectorAll("button[id^=unvote-candidate-]");
+	
+	unvoteButtons.forEach(function (button) {
+		button.addEventListener("click", function (e) {
+			e.preventDefault();
+			
+			voteIndexes = voteIndexes.filter(index => index != button.dataset.voterindex);
+			
+			button.hidden = true;
+			document.getElementById(`vote-candidate-${button.dataset.voternumber}`).hidden = false;
+			
+			voteRemainingCounter.textContent = ++numberOfVotesLeft;
+			
+			var disabledNonVotedCandidatesButtons = document.querySelectorAll("button[id^=vote-candidate-][disabled]");
+			
+			disabledNonVotedCandidatesButtons.forEach(button => button.disabled = false);
+			
+			submitVotesButton.disabled = true;
+			
+		});
+	});
+	
+	var votingOverlay = document.getElementById("voting-voted-overlay");
+	
+	submitVotesButton.addEventListener("click", function (e) {
+		
+		votingOverlay.classList.add("active");
+		
+	});
 	
 }
