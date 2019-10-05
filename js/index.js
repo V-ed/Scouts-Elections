737045d9224707dddc249f6eba767e3d1b8746c2
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-10-04 22:49:25 -0400

    Add homepage buttons design and small logic

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@ window.onbeforeunload = function(e) {
 	// Code that executes before reload
 	
 };
+
+var newElectionsButton = document.getElementById("home-new-button");
+
+newElectionsButton.addEventListener("click", e => {
+	
+	switch_view("setup-page")
+	
+});
