e1fbe9d70b18a6a749b29f393ceafca653a67956
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-10-04 23:32:58 -0400

    Add basic, non validated submit form data to setup page

--- js/setup.js
+++ js/setup.js
@@ -11,7 +11,7 @@ candidateAddButton.addEventListener("click", function (e) {
 	<div id="candidate-controls-${number}" class="form-group row">
 		<label class="col-sm-2 col-form-label" for="candidate-${number}">Candidat ${number}</label>
 		<div class="col-sm-10">
-			<input type="text" class="form-control" id="candidate-${number}" aria-describedby="candidate-${number}" placeholder="Nom">
+			<input type="text" class="form-control" id="candidate-${number}" aria-describedby="candidate-${number}" placeholder="Nom" name="candidate-${number}" data-candidatenumber="${number}">
 		</div>
 	</div>`);
 	
@@ -31,5 +31,32 @@ candidateRemoveButton.addEventListener("click", function (e) {
 	if (number == 2) {
 		candidateRemoveButton.hidden = true;
 	}
+	
+});
+
+var submitSetupButton = document.getElementById("setup-submit-button");
+
+submitSetupButton.addEventListener("click", e => {
+	e.preventDefault();
+	
+	var formData = new FormData(document.getElementById("setup-form"));
+	
+	var tempCandidates = [];
+	
+	var candidateData = formData.get("candidate-1");
+	
+	for(var i = 1; candidateData != null; i++){
+		tempCandidates.push({name: candidateData, voteCount: 0});
+		candidateData = formData.get(`candidate-${i}`);
+	}
+	
+	var data = {
+		dbName: formData.get("dbName"),
+		numberOfVoters: formData.get("numberOfVoters"),
+		numberOfVotePerVoter: formData.get("numberOfVotes"),
+		candidates: tempCandidates
+	};
+	
+	switch_view("voting-page", () => setup_voting_session(data));
 	
 });
