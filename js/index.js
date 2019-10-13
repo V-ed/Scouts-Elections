3909dc8c56b33be90d313a21f89883024fe09766
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-10-13 01:54:53 -0400

    Add drag and drop / file input handlers and add barebone JSON parse
    No validation on the data is done yet, but sends the user directly in the results page if no error during the JSON parse happens.

--- js/index.js
+++ js/index.js
@@ -11,3 +11,49 @@ newElectionsButton.addEventListener("click", e => switch_view("setup-page"));
 $(function () {
 	$(".is-popable").popover({trigger: "manual"});
 });
+
+var isAdvancedUpload = function() {
+	var divElement = document.createElement("div");
+	return (("draggable" in divElement) || ("ondragstart" in divElement && "ondrop" in divElement)) && "FormData" in window && "FileReader" in window;
+}();
+
+var $form = $("#database-loader-zone");
+
+if (isAdvancedUpload) {
+	
+	$form.addClass("has-advanced-upload");
+	
+	var droppedFiles = false;
+	
+	$form.on("drag dragstart dragend dragover dragenter dragleave drop", function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+	})
+	.on("dragover dragenter", function() {
+		$form.addClass("loader-is-dragover");
+	})
+	.on("dragleave dragend drop", function() {
+		$form.removeClass("loader-is-dragover");
+	})
+	.on("drop", e => load_file(e.originalEvent.dataTransfer.files[0]));
+	
+}
+
+var databaseLoaderInput = document.getElementById("loader-file-input");
+databaseLoaderInput.addEventListener("change", e => load_file(e.target.files[0]));
+
+function load_file(file) {
+	
+	file.text()
+	.then(text => {
+		
+		var data = JSON.parse(text);
+		
+		switch_view("pre-results-page", () => setup_pre_results_page(data));
+		
+	})
+	.catch(err => {
+		console.log("ya dun goofed!");
+	});
+	
+}
