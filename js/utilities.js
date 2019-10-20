c2edbabf6a38104420399ee1b5956952dee72878
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-10-20 16:08:49 -0400

    Fix using back / forward buttons using cache data
    Forcing a reload when using back / forward button force the javascript variables cache to reset, therefore fixing potential memory leaks in the app (example : "didDownloadDb" still being true)

--- js/utilities.js
+++ js/utilities.js
@@ -10,3 +10,8 @@ function download_data(data) {
 	didDownloadDb = true;
 	
 }
+
+// Reload if using back / forward button, therefore correctly cleaning the cache of variables
+if (window.performance && window.performance.navigation.type == window.performance.navigation.TYPE_BACK_FORWARD) {
+	document.location.reload(true);
+}
