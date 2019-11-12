08fe4e4e93d51ea2e494d883825c0912c04f7e57
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-11-11 21:05:43 -0500

    Tweak some more layouts and remove redundant classes
    For some reason there was multiple col-md-1 (3 times)  on a single element. wat.

--- js/setup.js
+++ js/setup.js
@@ -14,8 +14,8 @@ function setup_setup() {
 		
 		$(candidateContainer).append(`
 			<div id="candidate-controls-${number}" class="form-group row mb-2 mb-md-3">
-				<label class="col-sm-2 col-form-label" for="candidate-name-${number}">Candidat ${number}</label>
-				<div class="col-sm-10">
+				<label class="col-sm-3 col-md-2 col-form-label" for="candidate-name-${number}">Candidat ${number}</label>
+				<div class="col-sm-9 col-md-10">
 					<input type="text" class="form-control is-invalid is-popable" id="candidate-name-${number}" aria-describedby="candidate-name-${number}" placeholder="Nom" name="candidate-name-${number}" data-placement="top" data-candidatenumber="${number}" autocomplete="off" required>
 				</div>
 			</div>`);
