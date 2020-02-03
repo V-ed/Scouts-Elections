0d7612017a10fa7babb096e1bd5b27a812bdf21b
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-02-02 20:18:47 -0500

    Add fragment to project and load other views with it
    This allows the separation of each view into their own files which greatly helps developping as everything is separated as needed.
    
    The home page is still in the index to allow disabled javascript to still load the homepage, showing that javascript is disabled anyway, preventing any further actions.

--- js/index.js
+++ js/index.js
@@ -1,152 +1,156 @@
-const newElectionsButton = document.getElementById("home-new-button");
-const joinElectionsButton = document.getElementById("home-join-button");
-
-// Javascript enabled, enable inputs...
-
-newElectionsButton.disabled = false;
-document.getElementById("loader-file-input").disabled = false;
-document.getElementById("database-loader-zone").classList.remove("loader-disabled");
-
-// Index script
-
-newElectionsButton.addEventListener("click", () => switch_view("setup-page", () => setup_setup()));
-
-Utils.sendRequest(`${Utils.sharedElectionHostRoot}`, 'home-join-requester-container', false, 150).then(() => {
+function setup_index() {
 	
-	Utils.isServerAccessible = true;
+	const newElectionsButton = document.getElementById("home-new-button");
+	const joinElectionsButton = document.getElementById("home-join-button");
 	
-	joinElectionsButton.disabled = false;
+	// Javascript enabled, enable inputs...
 	
-	const codeElem = document.getElementById("fullCodeValue");
+	newElectionsButton.disabled = false;
+	document.getElementById("loader-file-input").disabled = false;
+	document.getElementById("database-loader-zone").classList.remove("loader-disabled");
 	
-	const modalButton = document.getElementById("home-join-election-modal-button");
+	// Index script
 	
-	const errorSpan = document.getElementById("home-join-modal-error-span");
+	newElectionsButton.addEventListener("click", () => switch_view("setup-page", () => setup_setup()));
 	
-	const partitionnedInputs = Array.from(document.querySelectorAll("input[data-partition-for-id='fullCodeValue']"));
-	
-	codeElem.addEventListener("change", () => {
-		modalButton.disabled = codeElem.value.length !== 6;
-		errorSpan.hidden = true;
-	});
-	
-	modalButton.addEventListener("click", async () => {
+	Utils.sendRequest(`${Utils.sharedElectionHostRoot}`, 'home-join-requester-container', false, 150).then(() => {
 		
-		const code = codeElem.value.toUpperCase();
+		Utils.isServerAccessible = true;
 		
-		modalButton.disabled = true;
-		partitionnedInputs.forEach(input => input.disabled = true);
-		errorSpan.hidden = true;
+		joinElectionsButton.disabled = false;
 		
-		const ajaxSettings = {
-			url: `${Utils.sharedElectionHostRoot}/join/${code}`,
-			contentType: 'application/javascript; charset=UTF-16',
-		};
+		const codeElem = document.getElementById("fullCodeValue");
 		
-		let request = Utils.sendRequest(ajaxSettings, 'home-join-modal-requester-container');
+		const modalButton = document.getElementById("home-join-election-modal-button");
 		
-		request.then(response => {
-			
-			return setup_votes(response.data, code, () => {
-				
-				$("#home-join-election-modal").modal("hide");
-				
-				errorSpan.hidden = true;
-				
-			}, 'home-join-modal-requester-container');
-			
-		})
-		.catch(response => {
-			
-			if (response.status == 400) {
-				errorSpan.textContent = "Ce code n'existe pas. Veuillez réessayer!";
-			}
-			else {
-				errorSpan.textContent = "Une erreur imprévue est survenue, veuillez réessayer!";
-			}
-			
-			errorSpan.hidden = false;
-			
-		})
-		.finally(() => {
-			modalButton.disabled = false;
-			partitionnedInputs.forEach(input => input.disabled = false);
+		const errorSpan = document.getElementById("home-join-modal-error-span");
+		
+		const partitionnedInputs = Array.from(document.querySelectorAll("input[data-partition-for-id='fullCodeValue']"));
+		
+		codeElem.addEventListener("change", () => {
+			modalButton.disabled = codeElem.value.length !== 6;
+			errorSpan.hidden = true;
 		});
 		
+		modalButton.addEventListener("click", async () => {
+			
+			const code = codeElem.value.toUpperCase();
+			
+			modalButton.disabled = true;
+			partitionnedInputs.forEach(input => input.disabled = true);
+			errorSpan.hidden = true;
+			
+			const ajaxSettings = {
+				url: `${Utils.sharedElectionHostRoot}/join/${code}`,
+				contentType: 'application/javascript; charset=UTF-16',
+			};
+			
+			let request = Utils.sendRequest(ajaxSettings, 'home-join-modal-requester-container');
+			
+			request.then(response => {
+				
+				return setup_votes(response.data, code, () => {
+					
+					$("#home-join-election-modal").modal("hide");
+					
+					errorSpan.hidden = true;
+					
+				}, 'home-join-modal-requester-container');
+				
+			})
+			.catch(response => {
+				
+				if (response.status == 400) {
+					errorSpan.textContent = "Ce code n'existe pas. Veuillez réessayer!";
+				}
+				else {
+					errorSpan.textContent = "Une erreur imprévue est survenue, veuillez réessayer!";
+				}
+				
+				errorSpan.hidden = false;
+				
+			})
+			.finally(() => {
+				modalButton.disabled = false;
+				partitionnedInputs.forEach(input => input.disabled = false);
+			});
+			
+		});
+		
+	}).catch(_error => {
+		// Nothing to do here in case of errors, the sendRequest method handles it all
 	});
 	
-}).catch(_error => {
-	// Nothing to do here in case of errors, the sendRequest method handles it all
-});
-
-Utils.create_file_loader("database-loader-zone", load_file, items => {
-	const count = items.length;
+	Utils.create_file_loader("database-loader-zone", load_file, items => {
+		const count = items.length;
+		
+		if (count > 1) {
+			return "Veuillez ne glisser qu'un seul fichier.";
+		}
+		else if (!is_file_json(items[0])) {
+			return "Le fichier n'est pas valide : seuls les fichiers \".json\" sont acceptés.";
+		}
+	});
 	
-	if (count > 1) {
-		return "Veuillez ne glisser qu'un seul fichier.";
+	function is_file_json(file) {
+		return file.type == "application/json";
 	}
-	else if (!is_file_json(items[0])) {
-		return "Le fichier n'est pas valide : seuls les fichiers \".json\" sont acceptés.";
+	
+	async function load_file(files) {
+		
+		const file = files[0];
+		
+		const text = await file.text();
+		
+		let data = undefined;
+		
+		try {
+			data = JSON.parse(text);
+		} catch (error) {
+			return "Une erreur est survenue lors du chargement du fichier : veuillez vous assurer que le fichier JSON est conforme.";
+		}
+		
+		const isValid = data.dbName !== undefined
+			&& data.numberOfVoters !== undefined
+			&& (data.numberOfVotePerVoter !== undefined || (data.numberOfVotePerVoterMin !== undefined && data.numberOfVotePerVoterMax !== undefined))
+			&& data.numberOfVoted !== undefined
+			&& data.hasSkipped !== undefined
+			&& data.candidates !== undefined;
+		
+		if (isValid) {
+			route_data(data);
+		}
+		else{
+			return "La base de données manque des informations cruciales - veuillez valider les données dans le fichier.";
+		}
+		
 	}
-});
-
-function is_file_json(file) {
-	return file.type == "application/json";
+	
+	function route_data(data) {
+		
+		if (data.hasSkipped || data.numberOfVoted == data.numberOfVoters) {
+			
+			Utils.isDownloadDisabled = true;
+			
+			setup_results(data);
+			
+		}
+		else {
+			
+			setup_votes(data);
+			
+		}
+		
+	}
+	
+	const preventDrag = e => {
+		e.preventDefault();
+		e.dataTransfer.effectAllowed = "none";
+		e.dataTransfer.dropEffect = "none";
+	};
+	
+	window.addEventListener("dragenter", preventDrag);
+	window.addEventListener("dragover", preventDrag);
+	window.addEventListener("drop", preventDrag);
+	
 }
-
-async function load_file(files) {
-	
-	const file = files[0];
-	
-	const text = await file.text();
-	
-	let data = undefined;
-	
-	try {
-		data = JSON.parse(text);
-	} catch (error) {
-		return "Une erreur est survenue lors du chargement du fichier : veuillez vous assurer que le fichier JSON est conforme.";
-	}
-	
-	const isValid = data.dbName !== undefined
-		&& data.numberOfVoters !== undefined
-		&& (data.numberOfVotePerVoter !== undefined || (data.numberOfVotePerVoterMin !== undefined && data.numberOfVotePerVoterMax !== undefined))
-		&& data.numberOfVoted !== undefined
-		&& data.hasSkipped !== undefined
-		&& data.candidates !== undefined;
-	
-	if (isValid) {
-		route_data(data);
-	}
-	else{
-		return "La base de données manque des informations cruciales - veuillez valider les données dans le fichier.";
-	}
-	
-}
-
-function route_data(data) {
-	
-	if (data.hasSkipped || data.numberOfVoted == data.numberOfVoters) {
-		
-		Utils.isDownloadDisabled = true;
-		
-		setup_results(data);
-		
-	}
-	else {
-		
-		setup_votes(data);
-		
-	}
-	
-}
-
-const preventDrag = e => {
-	e.preventDefault();
-	e.dataTransfer.effectAllowed = "none";
-	e.dataTransfer.dropEffect = "none";
-};
-
-window.addEventListener("dragenter", preventDrag);
-window.addEventListener("dragover", preventDrag);
-window.addEventListener("drop", preventDrag);
