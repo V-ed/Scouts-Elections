bdd76dd95da925673bc472e1773cdb940a4acac0
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-03-01 11:47:38 -0500

    Tweak input partition library logic to its own context
    This will allow me to abstract the generic methods between multiple implementations, while keeping the input partitionner module functions available outside.

--- js/input-partitionner.js
+++ js/input-partitionner.js
@@ -1,322 +1,326 @@
 const InputPartition = {};
 
-InputPartition.init = function (inputRoots) {
+;(function () {
 	
-	function getInputsFromRoot(inputRoot) {
-		
-		if (inputRoot.hasAttribute("data-length")) {
+	InputPartition.init = function (inputRoots) {
+	
+		function getInputsFromRoot(inputRoot) {
 			
-			const inputTemplate = inputRoot.querySelector("input");
-			
-			const inputsLength = parseInt(inputRoot.getAttribute("data-length"));
-			
-			inputRoot.innerHTML = "";
-			
-			for (let i = 0; i < inputsLength; i++) {
-				inputRoot.appendChild(inputTemplate.cloneNode(true));
+			if (inputRoot.hasAttribute("data-length")) {
+				
+				const inputTemplate = inputRoot.querySelector("input");
+				
+				const inputsLength = parseInt(inputRoot.getAttribute("data-length"));
+				
+				inputRoot.innerHTML = "";
+				
+				for (let i = 0; i < inputsLength; i++) {
+					inputRoot.appendChild(inputTemplate.cloneNode(true));
+				}
+				
 			}
 			
-		}
-		
-		return inputRoot.querySelectorAll("input");
-		
-	}
-	
-	Array.from(inputRoots).forEach(elem => {
-		
-		const inputs = getInputsFromRoot(elem);
-		
-		const hiddenInputValue = document.createElement("input");
-		hiddenInputValue.setAttribute("type", "hidden");
-		
-		const hiddenInputId = elem.hasAttribute("data-hidden-input-id") ? elem.getAttribute("data-hidden-input-id") : "partitionned-input".concat(elem.id ? "-".concat(elem.id) : "");
-		hiddenInputValue.setAttribute("id", hiddenInputId);
-		
-		elem.appendChild(hiddenInputValue);
-		
-		inputs.forEach(input => {
-			input.setAttribute("data-partition-for-id", hiddenInputId);
-			input.setAttribute("maxlength", 1);
-		});
-		
-		function updateHiddenInputValue() {
-			
-			hiddenInputValue.value = Array.from(inputs).map(input => input.value).join("");
-			
-			hiddenInputValue.dispatchEvent(new Event("input"));
+			return inputRoot.querySelectorAll("input");
 			
 		}
 		
-		inputs.forEach(input => {
+		Array.from(inputRoots).forEach(elem => {
 			
-			// Mobile fix
-			input.addEventListener("input", e => {
+			const inputs = getInputsFromRoot(elem);
+			
+			const hiddenInputValue = document.createElement("input");
+			hiddenInputValue.setAttribute("type", "hidden");
+			
+			const hiddenInputId = elem.hasAttribute("data-hidden-input-id") ? elem.getAttribute("data-hidden-input-id") : "partitionned-input".concat(elem.id ? "-".concat(elem.id) : "");
+			hiddenInputValue.setAttribute("id", hiddenInputId);
+			
+			elem.appendChild(hiddenInputValue);
+			
+			inputs.forEach(input => {
+				input.setAttribute("data-partition-for-id", hiddenInputId);
+				input.setAttribute("maxlength", 1);
+			});
+			
+			function updateHiddenInputValue() {
 				
-				if (e.inputType == "deleteContentBackward" || (e.inputType == "insertCompositionText" && !e.data)) {
+				hiddenInputValue.value = Array.from(inputs).map(input => input.value).join("");
+				
+				hiddenInputValue.dispatchEvent(new Event("input"));
+				
+			}
+			
+			inputs.forEach(input => {
+				
+				// Mobile fix
+				input.addEventListener("input", e => {
 					
-					input.value = "";
-					
-					const prevSibling = input.previousElementSibling;
-					
-					if (prevSibling) {
-						prevSibling.focus();
+					if (e.inputType == "deleteContentBackward" || (e.inputType == "insertCompositionText" && !e.data)) {
+						
+						input.value = "";
+						
+						const prevSibling = input.previousElementSibling;
+						
+						if (prevSibling) {
+							prevSibling.focus();
+						}
+						
+					}
+					else if (e.inputType == "insertCompositionText" || e.inputType == "insertText") {
+						
+						let canGoToNext = true;
+						
+						const inputPattern = input.getAttribute("pattern");
+						
+						if (inputPattern) {
+							
+							const charInput = e.data.length == 2 ? e.data.charAt(1) : e.data;
+							const prevValue = e.data.length == 2 ? e.data.charAt(0) : "";
+							
+							const patternRegex = new RegExp("^".concat(inputPattern, "$"));
+							
+							const doReplace = input.hasAttribute("data-do-replace") && input.getAttribute("data-do-replace") == "true";
+							
+							if (!patternRegex.test(charInput)) {
+								canGoToNext = false;
+								input.value = prevValue;
+							}
+							
+							if (canGoToNext && doReplace) {
+								input.value = charInput;
+							}
+							
+						}
+						
+						if (canGoToNext) {
+							
+							const sibling = input.nextElementSibling;
+							
+							if (sibling && !sibling.hasAttribute("hidden")) {
+								sibling.focus();
+							}
+							
+						}
+						
 					}
 					
-				}
-				else if (e.inputType == "insertCompositionText" || e.inputType == "insertText") {
+					updateHiddenInputValue();
 					
-					let canGoToNext = true;
+				});
+				
+				// Actual keyboard and semi mobile behavior
+				input.addEventListener("keyup", () => {
+					updateHiddenInputValue();
+				});
+				
+				input.addEventListener("keypress", e => {
 					
 					const inputPattern = input.getAttribute("pattern");
 					
 					if (inputPattern) {
 						
-						const charInput = e.data.length == 2 ? e.data.charAt(1) : e.data;
-						const prevValue = e.data.length == 2 ? e.data.charAt(0) : "";
+						const charInput = e.key || String.fromCharCode(e.keyCode);
 						
 						const patternRegex = new RegExp("^".concat(inputPattern, "$"));
 						
 						const doReplace = input.hasAttribute("data-do-replace") && input.getAttribute("data-do-replace") == "true";
 						
-						if (!patternRegex.test(charInput)) {
-							canGoToNext = false;
-							input.value = prevValue;
+						const valueToTest = doReplace ? charInput : input.value.concat(charInput);
+						
+						if (!patternRegex.test(valueToTest)) {
+							e.preventDefault();
+							e.stopPropagation();
+							return;
 						}
 						
-						if (canGoToNext && doReplace) {
+						if (doReplace) {
 							input.value = charInput;
+							e.preventDefault();
+							e.stopPropagation();
 						}
 						
 					}
 					
-					if (canGoToNext) {
-						
-						const sibling = input.nextElementSibling;
-						
-						if (sibling && !sibling.hasAttribute("hidden")) {
-							sibling.focus();
-						}
-						
+					const sibling = input.nextElementSibling;
+					
+					if (sibling) {
+						sibling.focus();
 					}
 					
-				}
+				});
 				
-				updateHiddenInputValue();
-				
-			});
-			
-			// Actual keyboard and semi mobile behavior
-			input.addEventListener("keyup", () => {
-				updateHiddenInputValue();
-			});
-			
-			input.addEventListener("keypress", e => {
-				
-				const inputPattern = input.getAttribute("pattern");
-				
-				if (inputPattern) {
+				input.addEventListener("keydown", e => {
 					
-					const charInput = e.key || String.fromCharCode(e.keyCode);
-					
-					const patternRegex = new RegExp("^".concat(inputPattern, "$"));
-					
-					const doReplace = input.hasAttribute("data-do-replace") && input.getAttribute("data-do-replace") == "true";
-					
-					const valueToTest = doReplace ? charInput : input.value.concat(charInput);
-					
-					if (!patternRegex.test(valueToTest)) {
+					if (e.key == "Backspace" || e.keyCode == 8) {
+						
 						e.preventDefault();
 						e.stopPropagation();
-						return;
-					}
-					
-					if (doReplace) {
-						input.value = charInput;
-						e.preventDefault();
-						e.stopPropagation();
-					}
-					
-				}
-				
-				const sibling = input.nextElementSibling;
-				
-				if (sibling) {
-					sibling.focus();
-				}
-				
-			});
-			
-			input.addEventListener("keydown", e => {
-				
-				if (e.key == "Backspace" || e.keyCode == 8) {
-					
-					e.preventDefault();
-					e.stopPropagation();
-					e.returnValue = false;
-					
-					let currentInput = input;
-					
-					do {
+						e.returnValue = false;
 						
-						currentInput.value = "";
+						let currentInput = input;
 						
-						const prevSibling = currentInput.previousElementSibling;
-						
-						if (prevSibling) {
-							prevSibling.focus();
-						}
-						
-						currentInput = prevSibling;
-						
-					} while (e.ctrlKey && currentInput);
-					
-				}
-				else if (e.key == "Delete" || e.keyCode == 46) {
-					
-					e.preventDefault();
-					e.stopPropagation();
-					e.returnValue = false;
-					
-					let currentInput = input;
-					
-					do {
-						
-						const sibling = currentInput.nextElementSibling;
-						
-						if (sibling && sibling.value) {
-							sibling.focus();
-						}
-						
-						currentInput = sibling;
-						
-						currentInput.value = "";
-						
-					} while (e.ctrlKey && currentInput);
-					
-				}
-				else if ((e.key == "ArrowLeft" || e.keyCode == 37) || (e.key == "Home" || e.keyCode == 36)) {
-					
-					e.preventDefault();
-					e.stopPropagation();
-					e.returnValue = false;
-					
-					let currentInput = input;
-					
-					do {
-						
-						const prevSibling = currentInput.previousElementSibling;
-						
-						if (prevSibling) {
-							prevSibling.focus();
-						}
-						
-						currentInput = prevSibling;
-						
-					} while ((e.ctrlKey || (e.key == "Home" || e.keyCode == 36)) && currentInput);
-					
-				}
-				else if ((e.key == "ArrowRight" || e.keyCode == 39) || (e.key == "End" || e.keyCode == 35)) {
-					
-					e.preventDefault();
-					e.stopPropagation();
-					e.returnValue = false;
-					
-					let currentInput = input;
-					
-					do {
-						
-						const sibling = currentInput.nextElementSibling;
-						
-						if (sibling && sibling.nextElementSibling && sibling.nextElementSibling.value) {
-							sibling.focus();
-						}
-						
-						currentInput = sibling;
-						
-					} while ((e.ctrlKey || (e.key == "End" || e.keyCode == 35)) && currentInput);
-					
-				}
-				
-			});
-			
-			input.addEventListener("focus", e => {
-				
-				let currentInput = input;
-				let prevSibling = undefined;
-				
-				do {
-					
-					if (!currentInput.value) {
-						
-						prevSibling = currentInput.previousElementSibling;
-						
-						if (prevSibling && !prevSibling.value) {
-							currentInput = prevSibling;
-						}
-						
-					}
-					
-				} while (prevSibling && !prevSibling.value);
-				
-				if (currentInput != input) {
-					currentInput.focus();
-				}
-				
-			});
-			
-			input.addEventListener("paste", e => {
-				
-				e.stopPropagation();
-				e.preventDefault();
-				
-				const clipboardData = e.clipboardData || window.clipboardData;
-				const pastedData = clipboardData.getData('Text');
-				
-				let didChangeValue = false;
-				
-				for (let i = 0, currentInput = input; currentInput && i < pastedData.length; i++) {
-					
-					let currentChar = pastedData[i];
-					
-					const inputPattern = currentInput.getAttribute("pattern");
-					
-					if (inputPattern) {
-						
-						const patternRegex = new RegExp("^".concat(inputPattern, "$"));
-						
-						while (!patternRegex.test(currentChar)) {
+						do {
 							
-							++i;
+							currentInput.value = "";
 							
-							if (i >= pastedData.length) {
-								return;
+							const prevSibling = currentInput.previousElementSibling;
+							
+							if (prevSibling) {
+								prevSibling.focus();
 							}
 							
-							currentChar = pastedData[i];
+							currentInput = prevSibling;
 							
-						}
+						} while (e.ctrlKey && currentInput);
+						
+					}
+					else if (e.key == "Delete" || e.keyCode == 46) {
+						
+						e.preventDefault();
+						e.stopPropagation();
+						e.returnValue = false;
+						
+						let currentInput = input;
+						
+						do {
+							
+							const sibling = currentInput.nextElementSibling;
+							
+							if (sibling && sibling.value) {
+								sibling.focus();
+							}
+							
+							currentInput = sibling;
+							
+							currentInput.value = "";
+							
+						} while (e.ctrlKey && currentInput);
+						
+					}
+					else if ((e.key == "ArrowLeft" || e.keyCode == 37) || (e.key == "Home" || e.keyCode == 36)) {
+						
+						e.preventDefault();
+						e.stopPropagation();
+						e.returnValue = false;
+						
+						let currentInput = input;
+						
+						do {
+							
+							const prevSibling = currentInput.previousElementSibling;
+							
+							if (prevSibling) {
+								prevSibling.focus();
+							}
+							
+							currentInput = prevSibling;
+							
+						} while ((e.ctrlKey || (e.key == "Home" || e.keyCode == 36)) && currentInput);
+						
+					}
+					else if ((e.key == "ArrowRight" || e.keyCode == 39) || (e.key == "End" || e.keyCode == 35)) {
+						
+						e.preventDefault();
+						e.stopPropagation();
+						e.returnValue = false;
+						
+						let currentInput = input;
+						
+						do {
+							
+							const sibling = currentInput.nextElementSibling;
+							
+							if (sibling && sibling.nextElementSibling && sibling.nextElementSibling.value) {
+								sibling.focus();
+							}
+							
+							currentInput = sibling;
+							
+						} while ((e.ctrlKey || (e.key == "End" || e.keyCode == 35)) && currentInput);
 						
 					}
 					
-					currentInput.value = currentChar;
-					didChangeValue = true;
+				});
+				
+				input.addEventListener("focus", e => {
 					
-					currentInput = currentInput.nextElementSibling;
+					let currentInput = input;
+					let prevSibling = undefined;
 					
-					if (currentInput) {
+					do {
+						
+						if (!currentInput.value) {
+							
+							prevSibling = currentInput.previousElementSibling;
+							
+							if (prevSibling && !prevSibling.value) {
+								currentInput = prevSibling;
+							}
+							
+						}
+						
+					} while (prevSibling && !prevSibling.value);
+					
+					if (currentInput != input) {
 						currentInput.focus();
 					}
 					
-				}
+				});
 				
-				if (didChangeValue) {
-					updateHiddenInputValue();
-				}
+				input.addEventListener("paste", e => {
+					
+					e.stopPropagation();
+					e.preventDefault();
+					
+					const clipboardData = e.clipboardData || window.clipboardData;
+					const pastedData = clipboardData.getData('Text');
+					
+					let didChangeValue = false;
+					
+					for (let i = 0, currentInput = input; currentInput && i < pastedData.length; i++) {
+						
+						let currentChar = pastedData[i];
+						
+						const inputPattern = currentInput.getAttribute("pattern");
+						
+						if (inputPattern) {
+							
+							const patternRegex = new RegExp("^".concat(inputPattern, "$"));
+							
+							while (!patternRegex.test(currentChar)) {
+								
+								++i;
+								
+								if (i >= pastedData.length) {
+									return;
+								}
+								
+								currentChar = pastedData[i];
+								
+							}
+							
+						}
+						
+						currentInput.value = currentChar;
+						didChangeValue = true;
+						
+						currentInput = currentInput.nextElementSibling;
+						
+						if (currentInput) {
+							currentInput.focus();
+						}
+						
+					}
+					
+					if (didChangeValue) {
+						updateHiddenInputValue();
+					}
+					
+				});
 				
 			});
 			
 		});
 		
-	});
+	}
 	
-}
+}());
