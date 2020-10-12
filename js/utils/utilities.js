bdd3b80fb1ba9a83676c8be01ff16b5ffd398e78
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-10-12 18:29:05 -0400

    Reformat utilities file and check if ClipboardJS is present

--- js/utils/utilities.js
+++ js/utils/utilities.js
@@ -5,52 +5,52 @@ class DataUtils {
     constructor() {
         this.resetVars();
     }
-	
+    
     /**
-	 *
-	 */
+     *
+     */
     resetVars() {
         // Set default variables
-		
+        
         this.isTouchDevice = 'ontouchstart' in document.documentElement;
-		
+        
         this.isDev = false && window.location.hostname.includes('localhost');
-		
+        
         this.sharedElectionHostRoot = this.isDev ? 'http://localhost:5678' : 'https://ved.ddnsfree.com/scouts-elections/api';
-		
+        
         this.isServerAccessible = false;
-		
+        
         // Setup downloadable database function
-		
+        
         this.didDownloadDb = false;
         this.isDownloadDisabled = false;
         this.dbIsDirty = false;
     }
-	
+    
     /**
-	 *
-	 * @param {boolean} [doPreventVarsReset]
-	 */
+     *
+     * @param {boolean} [doPreventVarsReset]
+     */
     init(doPreventVarsReset) {
         if (!doPreventVarsReset) {
             this.resetVars();
         }
-		
+        
         // --------------------------------
         // Setup popovers
         // --------------------------------
-		
+        
         // Accept buttons in tooltips and Popovers
         // @ts-ignore
         $.fn.tooltip.Constructor.Default.whiteList.button = [];
-		
+        
         $('.is-popable').popover({trigger: 'manual'});
         $('.is-popable-hover').popover({trigger: 'hover'});
-		
+        
         // --------------------------------
         // Initialize acceptance forms
         // --------------------------------
-		
+        
         Array.from(document.getElementsByClassName('acceptance-form-div-accept-button')).forEach(button => {
             button.addEventListener('click', () => {
                 // @ts-ignore
@@ -59,32 +59,32 @@ class DataUtils {
                 Array.from(document.getElementsByClassName('accepted-server-div')).forEach(div => div.hidden = false);
             });
         });
-		
+        
         // --------------------------------
         // Miscellaneous
         // --------------------------------
-		
+        
         this.setLabelNonClickable(document.querySelectorAll('label.col-form-label'));
-		
+        
         InputPartition.init(document.querySelectorAll('div.row.input-partition'));
-		
+        
         // Restore current focus when changing screen orientation
         // Especially useful for mobile
-		
+        
         window.addEventListener('orientationchange', function() {
             setTimeout(() => {
                 document.activeElement.scrollIntoView();
             }, 500);
         });
-		
+        
         // Workaround to fix Chrome's device orientation issue : https://github.com/V-ed/Scouts-Elections/issues/65
-		
+        
         // @ts-ignore
         if (window.chrome) {
             window.addEventListener('orientationchange', function() {
                 if (screen.orientation.angle % 180 == 0) {
                     const bodyElem = document.querySelector('body');
-
+                    
                     bodyElem.classList.toggle('d-flex');
                     setTimeout(() => {
                         bodyElem.classList.toggle('d-flex');
@@ -92,83 +92,85 @@ class DataUtils {
                 }
             });
         }
-		
+        
         // Reload if using back / forward button, therefore correctly cleaning the cache of variables
-		
+        
         if (window.performance && window.performance.navigation.type == window.performance.navigation.TYPE_BACK_FORWARD) {
             document.location.reload(true);
         }
-		
-        const clipboardJS = new ClipboardJS('.copy-button');
-		
-        clipboardJS.on('success', function(e) {
-            e.clearSelection();
-			
-            e.trigger.setAttribute('data-content', e.trigger.getAttribute('data-popup-clip-success-msg'));
-			
-            $(e.trigger).popover('show');
-			
-            setTimeout(() => {
-                $(e.trigger).popover('hide');
-            }, 1000);
-        });
-		
-        clipboardJS.on('error', function(e) {
-            e.trigger.setAttribute('data-content', e.trigger.getAttribute('data-popup-clip-error-msg'));
-			
-            $(e.trigger).popover('show');
-			
-            setTimeout(() => {
-                $(e.trigger).popover('hide');
-            }, 2000);
-        });
-		
+        
+        if (typeof ClipboardJS !== 'undefined') {
+            const clipboardJS = new ClipboardJS('.copy-button');
+            
+            clipboardJS.on('success', function(e) {
+                e.clearSelection();
+                
+                e.trigger.setAttribute('data-content', e.trigger.getAttribute('data-popup-clip-success-msg'));
+                
+                $(e.trigger).popover('show');
+                
+                setTimeout(() => {
+                    $(e.trigger).popover('hide');
+                }, 1000);
+            });
+            
+            clipboardJS.on('error', function(e) {
+                e.trigger.setAttribute('data-content', e.trigger.getAttribute('data-popup-clip-error-msg'));
+                
+                $(e.trigger).popover('show');
+                
+                setTimeout(() => {
+                    $(e.trigger).popover('hide');
+                }, 2000);
+            });
+        }
+        
         this.hideSharedCodes();
     }
-	
+    
     // ---------------------------------------------
     // 				DATA DOWNLOAD
     // ---------------------------------------------
-	
+    
     /**
-	 *
-	 */
+     *
+     */
     shouldDownloadData() {
         if (this.isDownloadDisabled) {
             return false;
         }
         return this.dbIsDirty || !this.didDownloadDb;
     }
-	
+    
     /**
-	 *
-	 * @param {string | ElectionData} data
-	 * @param {string} [dbNameSuffix]
-	 */
+     *
+     * @param {string | ElectionData} data
+     * @param {string} [dbNameSuffix]
+     */
     downloadData(data, dbNameSuffix) {
         const stringData = data instanceof ElectionData ? data.getAsJSON() : data;
-		
+        
         const dbName = data instanceof ElectionData ? data.dbName : JSON.parse(data).dbName;
-		
+        
         const parsedDbNameSuffix = dbNameSuffix || '';
-		
+        
         const file = new File([stringData], `${dbName}${parsedDbNameSuffix}.json`, {type: 'application/json;charset=utf-8'});
 
         saveAs(file);
-		
+        
         this.didDownloadDb = true;
         this.dbIsDirty = false;
     }
-	
+    
     // ---------------------------------------------
     // 				IMAGE HANDLING
     // ---------------------------------------------
-	
+    
     /**
-	 * Load image on all elements matching under given view id
-	 * @param {string} viewId
-	 * @param {string} imageData
-	 */
+     * Load image on all elements matching under given view id
+     * @param {string} viewId
+     * @param {string} imageData
+     */
     initializeImages(viewId, imageData) {
         if (imageData) {
             const uncompressedImage = LZString.decompressFromUTF16(imageData);
@@ -181,72 +183,72 @@ class DataUtils {
             });
         }
     }
-	
+    
     /**
-	 *
-	 * @param {string} viewId
-	 */
+     *
+     * @param {string} viewId
+     */
     uninitializeImages(viewId) {
         this.viewImageIterator(viewId, imageElem => imageElem.src = '');
     }
-	
+    
     /**
-	 *
-	 * @param {string} viewId
-	 * @param {(imageElem: HTMLImageElement) => *} iteratorFn
-	 */
+     *
+     * @param {string} viewId
+     * @param {(imageElem: HTMLImageElement) => *} iteratorFn
+     */
     viewImageIterator(viewId, iteratorFn) {
         const imageContainers = document.getElementById(viewId).querySelectorAll('div.election-group-image');
-		
+        
         imageContainers.forEach(container => {
             const imageElems = container.querySelectorAll('img');
-			
+            
             imageElems.forEach(imageElem => iteratorFn(imageElem));
             // imageElem.src = imageData
         });
     }
-	
+    
     /**
-	 *
-	 * @param {string} viewId
-	 * @param {(container: Element) => *} iteratorFn
-	 */
+     *
+     * @param {string} viewId
+     * @param {(container: Element) => *} iteratorFn
+     */
     viewImageContainerIterator(viewId, iteratorFn) {
         const imageContainers = document.getElementById(viewId).querySelectorAll('div.election-group-image');
-		
+        
         imageContainers.forEach(container => iteratorFn(container));
     }
-	
+    
     // ---------------------------------------------
     // 				SHARED ELECTIONS
     // ---------------------------------------------
-	
+    
     /**
-	 *
-	 */
+     *
+     */
     hideSharedCodes() {
         document.querySelectorAll('.shared-election-container').forEach(/** @param {HTMLElement} elem */ (elem) => this.handleSharedCodeHiddenStatus(elem, true));
         document.querySelectorAll('.non-shared-election-container').forEach(/** @param {HTMLElement} elem */ (elem) => this.handleSharedCodeHiddenStatus(elem, false));
     }
-	
+    
     /**
-	 *
-	 * @param {string} sharedElectionCode
-	 */
+     *
+     * @param {string} sharedElectionCode
+     */
     showSharedCode(sharedElectionCode) {
         document.querySelectorAll('.shared-election-code').forEach(elem => elem.textContent = sharedElectionCode);
         document.querySelectorAll('.shared-election-container').forEach(/** @param {HTMLElement} elem */ (elem) => this.handleSharedCodeHiddenStatus(elem, false));
         document.querySelectorAll('.non-shared-election-container').forEach(/** @param {HTMLElement} elem */ (elem) => this.handleSharedCodeHiddenStatus(elem, true));
     }
-	
+    
     /**
-	 *
-	 * @param {HTMLElement} element
-	 * @param {boolean} doHide
-	 */
+     *
+     * @param {HTMLElement} element
+     * @param {boolean} doHide
+     */
     handleSharedCodeHiddenStatus(element, doHide) {
         element.hidden = doHide;
-		
+        
         if (element.classList.contains('d-flex')) {
             element.classList.remove('d-flex');
             element.setAttribute('data-shared-did-flex', 'true');
@@ -255,18 +257,18 @@ class DataUtils {
             element.removeAttribute('data-shared-did-flex');
         }
     }
-	
+    
     // ---------------------------------------------
     // 				COMMON UTILITIES
     // ---------------------------------------------
-	
+    
     /**
-	 * Set bootstrap labels to not focus input on click
-	 * @param {HTMLLabelElement | HTMLLabelElement[] | NodeListOf<HTMLLabelElement>} labels
-	 */
+     * Set bootstrap labels to not focus input on click
+     * @param {HTMLLabelElement | HTMLLabelElement[] | NodeListOf<HTMLLabelElement>} labels
+     */
     setLabelNonClickable(labels) {
         const labelsArray = labels instanceof NodeList ? Array.from(labels) : Array.isArray(labels) ? labels : [labels];
-		
+        
         labelsArray.forEach(label => {
             label.addEventListener('click', e => {
                 e.preventDefault();
